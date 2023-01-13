b17862d96a721cea40ad798c14f483405220948c
Amy <amyrosamond@gmail.com>
2023-01-13 17:51:32 +0000

    pictures added and project page started

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function cardHover() {
+    var card = document.querySelectorAll('.cardHover');
+    var cardBack = document.querySelectorAll('.cardBack');
+    var cardImg = document.querySelectorAll('.cardImg');
+    var cardTxt = document.querySelectorAll('.cardTxt');
+
+    card.classList.add('.card-hover');
+    cardBack.classList.add('.card-back-show');
+    cardImg.classList.add('.card-back-hide');
+    cardTxt.classList.remove('.card-back-text-hide');
+}
+
+function cardNoHover() {
+    var card = document.querySelectorAll('.cardHover');
+
+    card.classList.remove('.card-hover');
+    cardBack.classList.remove('.card-back-show');
+    cardImg.classList.remove('.card-back-hide');
+    cardTxt.classList.add('.card-back-text-hide');
+}
